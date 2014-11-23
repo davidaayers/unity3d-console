574d6e0493c047ba0890a6c335e8833273d4d81e
NikB <nikb@mac.com>
2014-11-23 13:18:49 -0600

    Change console toggle to shift + backquote (tilde)

--- Console/Scripts/ConsoleToggler.cs
+++ Console/Scripts/ConsoleToggler.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class ConsoleToggler : MonoBehaviour {
     private bool consoleEnabled = false;
+	private bool shiftDown = false;
     public ConsoleAction ConsoleOpenAction;
     public ConsoleAction ConsoleCloseAction;
 
     void Update () {
-        if (Input.GetKeyDown(KeyCode.BackQuote)) {
+		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
+			shiftDown = true;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift)) {
+			shiftDown = false;
+		}
+		if ((shiftDown || consoleEnabled) && Input.GetKeyDown(KeyCode.BackQuote)) {
             ToggleConsole();
         }
     }
